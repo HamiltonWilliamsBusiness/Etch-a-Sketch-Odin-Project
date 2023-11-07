84cb5e982449cf735fcbeef62c9628b12436f4a2
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2023-11-07 14:46:17 -0600

    feat: complete for now

--- myscripts.js
+++ myscripts.js
@@ -21,6 +21,48 @@ function func3() {
     color = "white";
 }
 
+// Function to remove all div elements from the container
+function clearGrid() {
+    // Remove all child elements (divs) from the container
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+function func4() {
+
+    clearGrid()
+    let numDivs = slider.value * slider.value
+    let divSize = 500 / numDivs;
+    console.log(slider.value)
+    console.log(slider.value * slider.value)
+    for (let i = 1; i <= numDivs; i++) {
+        let colorChange = document.createElement('div');
+        // colorChange.classList.add("colorSwap");
+        colorChange.style.width = divSize + "px";
+        colorChange.style.height = divSize + "px";
+        colorChange.style.flex = "1 1 auto";
+        colorChange.style.border = "2px solid red";
+        colorChange.style.boxSizing = "border-box";
+        colorChange.style.margin = "0";
+        colorChange.style.padding = "0";
+
+        colorChange.addEventListener('mouseover', () => {
+            if (color === "white") {
+                colorChange.style.backgroundColor = "white";
+            } if (color === "red") {
+                colorChange.style.backgroundColor = "red";
+            } if (color === "green") {
+                colorChange.style.backgroundColor = "green";
+            }
+        });
+
+        container.appendChild(colorChange);
+    }
+
+}
+
 const buttonColor = document.getElementById("colorChanger");
 
 buttonColor.addEventListener('click', func)
@@ -32,6 +74,24 @@ buttonColor2.addEventListener('click', func2)
 const buttonColor3 = document.getElementById("colorChanger3");
 
 buttonColor3.addEventListener('click', func3)
+
+// const sizeChange = document.getElementById("sizeChange");
+
+// sizeChange.addEventListener('click', func4)
+
+// Get references to the slider input and the paragraph to display the value
+// const slider = document.getElementById("myRange");
+// const sliderValue = document.getElementById("sliderValue");
+
+// Display the initial value
+// sliderValue.textContent = "Value: " + slider.value;
+
+// Add an event listener to update the displayed value when the slider changes
+// slider.addEventListener("input", function () {
+//     sliderValue.textContent = "Value: " + slider.value;
+// });
+
+// slider.addEventListener("input", func4);
 
 
 // Creating 16x16 grid of square divs
@@ -55,6 +115,9 @@ for (let i = 1; i <= 256; i++) {
     container.appendChild(colorChange);
     divElements.push(colorChange);
 }
+
+
+
 
 // for (let i = 1; i <= 256; i++) {
 //     let colorChange = document.createElement('div');
