4a1095b348eeeef44c7e2c3eade01b337c35e420
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2024-03-06 19:33:34 -0600

    Feat: Cleaned code

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,8 @@
 const container = document.querySelector('#holder');
-const divElements = []; // Create an array to store the div elements
 
-// let colorChange = document.createElement('div');
-// colorChange.addEventListener('click', func);
-// colorChange.classList.add("colorSwap");
+// Create an array to store the div elements
+const divElements = []; 
 
-// container.append(colorChange);
 let nameClass = "colorSwap2";
 let color = "white"
 
@@ -28,40 +25,7 @@ function clearGrid() {
         container.removeChild(container.firstChild);
     }
 }
-
-
-function func4() {
-
-    clearGrid()
-    let numDivs = slider.value * slider.value
-    let divSize = 500 / numDivs;
-    console.log(slider.value)
-    console.log(slider.value * slider.value)
-    for (let i = 1; i <= numDivs; i++) {
-        let colorChange = document.createElement('div');
-        // colorChange.classList.add("colorSwap");
-        colorChange.style.width = divSize + "px";
-        colorChange.style.height = divSize + "px";
-        colorChange.style.flex = "1 1 auto";
-        colorChange.style.border = "2px solid red";
-        colorChange.style.boxSizing = "border-box";
-        colorChange.style.margin = "0";
-        colorChange.style.padding = "0";
-
-        colorChange.addEventListener('mouseover', () => {
-            if (color === "white") {
-                colorChange.style.backgroundColor = "white";
-            } if (color === "red") {
-                colorChange.style.backgroundColor = "red";
-            } if (color === "green") {
-                colorChange.style.backgroundColor = "green";
-            }
-        });
-
-        container.appendChild(colorChange);
-    }
-
-}
+// buttons
 
 const buttonColor = document.getElementById("colorChanger");
 
@@ -75,28 +39,34 @@ const buttonColor3 = document.getElementById("colorChanger3");
 
 buttonColor3.addEventListener('click', func3)
 
-// const sizeChange = document.getElementById("sizeChange");
+function func4(){
+    let userInput = prompt('Enter a value 1-100');
+    // alert(userInput)
+    aSizeChange()
 
-// sizeChange.addEventListener('click', func4)
+    for (let i = 1; i <= userInput; i++) {
+        let colorChange = document.createElement('div');
+        colorChange.classList.add("colorSwap");
+    
+        colorChange.addEventListener('mouseover', () => {
+            if (color === "white") {
+                colorChange.style.backgroundColor = "white";
+            } if (color === "red") {
+                colorChange.style.backgroundColor = "red";
+            } if (color === "green") {
+                colorChange.style.backgroundColor = "green";
+            }
+        });
+    
+        container.appendChild(colorChange);
+        divElements.push(colorChange);
+    }
+}
 
-// Get references to the slider input and the paragraph to display the value
-// const slider = document.getElementById("myRange");
-// const sliderValue = document.getElementById("sliderValue");
+const sizeChanger = document.querySelector('#sizeChanger');
+sizeChanger.addEventListener('click', func4)
 
-// Display the initial value
-// sliderValue.textContent = "Value: " + slider.value;
-
-// Add an event listener to update the displayed value when the slider changes
-// slider.addEventListener("input", function () {
-//     sliderValue.textContent = "Value: " + slider.value;
-// });
-
-// slider.addEventListener("input", func4);
-
-
-// Creating 16x16 grid of square divs
-
-// Loop that iterates 256 times
+// Creating 16x16 grid of square divs & Loop that iterates 256 times
 
 for (let i = 1; i <= 256; i++) {
     let colorChange = document.createElement('div');
@@ -116,18 +86,16 @@ for (let i = 1; i <= 256; i++) {
     divElements.push(colorChange);
 }
 
+function aSizeChange(){
+    // Delete Old Grid
+    container.innerHTML = ''; // This will remove all child elements inside the container
+
+
+    // Reset Old Div Array Value
+    divElements = []
+
+}
 
 
 
-// for (let i = 1; i <= 256; i++) {
-//     let colorChange = document.createElement('div');
-//     colorChange.classList.add("colorSwap");
-
-//     colorChange.addEventListener('mouseover', () => {
-//         colorChange.classList.contains(nameClass) ? colorChange.classList.toggle("colorSwap") : colorChange.classList.toggle(nameClass)
-//     });
-
-//     container.appendChild(colorChange);
-//     divElements.push(colorChange); // Add the div to the array
-// }
 
